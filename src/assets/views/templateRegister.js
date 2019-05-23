711b7034e61ec9e5b26293d9415d16a415262b2e
vurles <vurles27@gmail.com>
2019-05-23 14:32:41 -0400

    templateRegistry is changed by templateRegister

--- src/assets/views/templateRegister.js
+++ src/assets/views/templateRegister.js
@@ -18,13 +18,19 @@ export const templateRegister =()=>{
                     <div class="input-group-prepend">
                         <span class="input-group-text">Nombre y Apellido</span>
                     </div>
-                    <input type="text" aria-label="Nombre" class="form-control">
-                    <input type="text" aria-label="Apellido" class="form-control">
+                    <input type="text" placeholder="Nombre" class="form-control">
+                    <input type="text" placeholder="Apellido" class="form-control">
+                    <input type="number" placeholder="Cedúla de identidad" class="form-control">
+                    <input type="mail" placeholder="example@lab.com" class="form-control">
+                    <input type="number" placeholder="+56 9123 45 678" class="form-control">
                 </div>
-            </div>
+                <div id="rut">
+                    
+                </div>
+                </div>
         </div>
     </div>
-    </main>` 
+   ` 
 
     containerRegister.innerHTML=contenRegister;
         return containerRegister;  
