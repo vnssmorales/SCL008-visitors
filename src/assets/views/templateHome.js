cda907ec463dad8cfe34b3d9834696d1ac8fe4df
vnssmorales <vnss.morales@gmail.com>
2019-05-23 16:08:32 -0400

    agregando css a template home

--- src/assets/views/templateHome.js
+++ src/assets/views/templateHome.js
@@ -2,14 +2,19 @@ export const templateHome =()=>{
 
     const containerHome = document.createElement('div');
 
-    const contentHome = `<main>
+    const contentHome = `
+     
+    <div class= "card text-center id="visitors" 
+    <div class= "card-header" </div>
     <h1>Bienvenido a Visitors</h1>
-    <div class="visitant"> 
+    <div class= "card-body" 
     <button class="btn btn-outline-dark" id="register">Registro</button> 
     <button class="btn btn-outline-dark" id="login">Ingreso</button> 
-    </div>
     <button class="btn btn-outline-dark" id="admin">Administrador</button>
-    </main>` 
+    </div>
+    </div>
+    
+    ` 
     containerHome.innerHTML=contentHome;
      const btnRegister=containerHome.querySelector('#register');
      const btnLogin=containerHome.querySelector('#login');
