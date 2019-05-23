10d356b282d85ceeb17895a3891d613a7e9a10db
vurles <vurles27@gmail.com>
2019-05-23 18:07:02 -0400

    save checkIn

--- src/assets/js/checkIn.js
+++ src/assets/js/checkIn.js
@@ -4,13 +4,10 @@ export const registration = (name, lastname, mail, identityCell, phone) => {
     firebase.auth().createUserWithEmailAndPassword(mail, identityCell)
     .then(function(){
     
-     
     })
     .catch(function(error) {
-    
         let errorCode =alert (error.code);
         let errorMessage = alert(error.message);
-   
   });
   firebase.auth().onAuthStateChanged(function(user) { 
     if (user) { // si esta activo
