e36098dd2c116d11b1503f6dc95e836635ba9be5
vnssmorales <vnss.morales@gmail.com>
2019-05-24 15:08:46 -0400

    template login modificado

--- src/assets/views/templateLogin.js
+++ src/assets/views/templateLogin.js
@@ -5,13 +5,16 @@ export const templateLogin =()=>{
     // creamos el contenido del login
     const contentLogin = `
     <div class="container" id="loginreg">
-    <div class="row around-xs">
-    <div class="foto col-xs-10 col-sm-8 col-md-8">
-    <div class="input"> <input id="user" type="user" placeholder="ingresa usuario"> 
+    <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12" id="ingusuario">
+    <div> <input id="user" type="user" placeholder="ingresa usuario"> 
     <p class="error" id="errorNameActive"> </div>
-    <div class="input"> <input id="pass" type="password" placeholder="ingresar contraseña">
-    <p class="error" id="errorPassActive"></p> </div>
-    <div class="btn btn-outline-dark"> <button class="btn" id="userActive">Ingresar</button> 
+    <div> <input id="pass" type="password" placeholder="ingresar contraseña">
+    <p class="error" id="errorPassActive"></p></div>
+    </div>
+    <div class="col-sm-12 col-md-12 col-lg-12" id="btninguser">
+    <div> <button class="btn btn-outline-dark" id="userActive">Ingresar</button> 
+    </div>
     </div>
     </div>
     </div>
